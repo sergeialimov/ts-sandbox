294abece25d09c09bf06a41afabb0f3065939529
sergeialimov <8912652@gmail.com>
2022-03-31 18:16:01 +0300

    add configs

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-expressions': [2, { allowShortCircuit: true }],
+    'no-multiple-empty-lines': [2, { max: 2 }],
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
