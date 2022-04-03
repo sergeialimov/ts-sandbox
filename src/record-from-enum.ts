74b20f7e64fc5b38ecb6c2a267d8c3af7ed35201
sergeialimov <8912652@gmail.com>
2022-04-03 09:21:06 +0300

    remove details

--- src/record-from-enum.ts
+++ src/record-from-enum.ts
@@ -1,9 +1,9 @@
-enum AudienceTypeEnum {
-  NPI = 'npi',
-  COUNTRY = 'country',
-  INSTITUTION = 'institution',
-  LAL = 'lal',
+enum someEnum {
+  KEY1 = 'value1',
+  KEY2 = 'value2',
+  KEY3 = 'value3',
+  KEY4 = 'value4',
 }
 
-type NewType = Record<Exclude<AudienceTypeEnum, 'lal'>, 'basePairingServiceType'>;
-type NewType2 = Record<AudienceTypeEnum, 'basePairingServiceType'>;
+type NewType = Record<Exclude<someEnum, 'value1'>, 'basePairingServiceType'>;
+type NewType2 = Record<someEnum, 'basePairingServiceType'>;
