3150907370db158bd42e04b0cd69bbcd13b191ba
sergeialimov <8912652@gmail.com>
2022-06-19 13:50:29 +0300

    move logic to db.service

--- src/tasks/refactoring/csv-user-report-generator/db.service.ts
+++ src/tasks/refactoring/csv-user-report-generator/db.service.ts
@@ -1,7 +1,10 @@
+import { User, UserRegistrationInfo } from './types';
+
 // where is a connection to the db?
 
 export default class DbService {
-  static async getUsersIdsFromDB(nameRegex: string) : User[] {
+  static async getUsersIdsFromDB(nameFilter: string) : User[] {
+    // const nameRegex = new RegExp(nameFilter);
     // repace with orm param or sql query – .filter((u) => nameRegex.test(u.name));
     // stub, gets users from DB
     // return ids
