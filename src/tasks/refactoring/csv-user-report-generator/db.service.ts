de7aa423ced32857bd8cb747cc9430ded58bf339
sergeialimov <8912652@gmail.com>
2022-06-19 14:21:50 +0300

    add Promise to returned types

--- src/tasks/refactoring/csv-user-report-generator/db.service.ts
+++ src/tasks/refactoring/csv-user-report-generator/db.service.ts
@@ -3,15 +3,14 @@ import { User, UserRegistrationInfo } from './types';
 // where is a connection to the db?
 
 export default class DbService {
-  static async getUsersIdsFromDB(nameFilter: string) : User[] {
+  static async getUsersFromDB(nameFilter: string) : Promise<User[]> {
     // const nameRegex = new RegExp(nameFilter);
     // repace with orm param or sql query – .filter((u) => nameRegex.test(u.name));
     // stub, gets users from DB
-    // return ids
     return [];
   }
 
-  static async getUsersRegistrationInfo(userIds: string[]) : UserRegistrationInfo[] {
+  static async getUsersRegistrationInfo(userIds: string[]) : Promise<UserRegistrationInfo[]> {
     // stub, gets users registration info from external service
     return [];
   }
